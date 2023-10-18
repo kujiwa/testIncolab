8aa0085df970f11c644a13c9c5ac3ba7a66d709c
kujiwa <57786874+kujiwa@users.noreply.github.com>
2023-10-18 23:08:53 +0800

    Update yjh_sizeTest.py

--- amp_extensions/afm/yjh_sizeTest.py
+++ amp_extensions/afm/yjh_sizeTest.py
@@ -2,12 +2,18 @@ import torch
 import numpy as np
 from torch.utils.data.dataloader import default_collate
 from csrc import _C
+
 height=16
 width =16
 import torch
 
-lines = torch.Tensor([[0, 0, 1, 1], [1, 4, 2, 3], [4, 4, 8, 7]])
+device='cuda'
+lines = torch.Tensor([[0, 0, 1, 1], [1, 4, 2, 3], [4, 4, 8, 7]]).to(device)
 print(lines)
 
 print(_C)
-lmap, _, _ = _C.encodels(lines,height,width,height,width,lines.size(0))
+lmap, labels, _ = _C.encodels(lines,height,width,height,width,lines.size(0))
+print(lmap)
+print(labels)
+print(lmap.size())
+print(labels.size())
