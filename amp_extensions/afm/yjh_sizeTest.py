91a9a153af4241fc6e53e71cec6104cbb07a7dcd
kujiwa <57786874+kujiwa@users.noreply.github.com>
2023-10-18 23:12:35 +0800

    Update yjh_sizeTest.py

--- amp_extensions/afm/yjh_sizeTest.py
+++ amp_extensions/afm/yjh_sizeTest.py
@@ -10,10 +10,9 @@ import torch
 device='cuda'
 lines = torch.Tensor([[0, 0, 1, 1], [1, 4, 2, 3], [4, 4, 8, 7]]).to(device)
 print(lines)
-
 print(_C)
 lmap, labels, _ = _C.encodels(lines,height,width,height,width,lines.size(0))
 print(lmap)
 print(labels)
-print(lmap.size())
-print(labels.size())
+print(lmap.size())#(6,height,width)
+print(labels.size())#(3,height,width)
